009010092035d783879fb401b3f8ed5c6a152c81
jacobStankaitis <jokubas.stankaitis@gmail.com>
2018-02-18 13:45:26 +0200

    Daug visko

--- plk/urls.py
+++ plk/urls.py
@@ -1,5 +1,4 @@
 """plk URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
@@ -16,6 +15,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from web.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
 ]
--- web/views.py
+++ web/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from web.models import Student
+
+
+def index(request):
+    students = Student.objects.all()
+    return render(request, 'index.html', {'students': students})
