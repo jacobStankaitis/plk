3e1b87012393d528ec28f32efa500e129b66f3e6
jacobStankaitis <jokubas.stankaitis@gmail.com>
2018-02-19 13:14:13 +0200

    Design&content

--- plk/urls.py
+++ plk/urls.py
@@ -16,8 +16,10 @@ from django.contrib import admin
 from django.urls import path
 
 from web.views import index
+from django.conf import settings
 
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
