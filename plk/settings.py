7a068cc3bb8466b362a949e03683066a828dc4a5
jacobStankaitis <jokubas.stankaitis@gmail.com>
2018-02-21 09:54:29 +0200

    ngnix

--- plk/settings.py
+++ plk/settings.py
@@ -110,10 +110,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = 'static/'
 
 STATIC_URL = "/static/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_ROOT = 'media/'
 
 MEDIA_URL = "/media/"
